e99b6402bcdae856118ffabf1adec90a7e9924e4
Underewarrr <rafhaelxd@gmail.com>
2023-05-23 13:32:47 -0300

    Refac: Leagues fetch Logic
    Dessa forma, os dados dos países serão buscados apenas uma vez na montagem do componente e as renderizações subsequentes usarão os dados do local storage

--- src/pages/user/panel/Leagues.jsx
+++ src/pages/user/panel/Leagues.jsx
@@ -6,6 +6,7 @@ const Leagues = ({ setLeagueId }) => {
   const [selectedLeague, setSelectedLeague] = useState('');
   const [selectedCountry, setSelectedCountry] = useState('');
   const [leagues, setLeagues] = useState([]);
+  const [dataFetched, setDataFetched] = useState(false); // Flag to track data fetch
 
   useEffect(() => {
     // Make API request to fetch the list of countries
@@ -21,28 +22,26 @@ const Leagues = ({ setLeagueId }) => {
     }
   }, [selectedCountry]);
 
-  /* // Example 
-  useEffect(() => {
-    if (selectedLeague) {
-      // Make API request to fetch teams for the selected league
-      console.log(selectedLeague)
-    } else {
-      console.Console()
-    }
-  }, [selectedLeague]);
- */
   const fetchCountries = async () => {
     try {
-      const apiKey = localStorage.getItem('apiKey');
-      console.log('Fetching countries...');
-      const response = await axios.get('https://v3.football.api-sports.io/countries', {
-        headers: {
-          'x-rapidapi-host': 'v3.football.api-sports.io',
-          'x-rapidapi-key': apiKey || '',
-        },
-      });
-      console.log('Countries:', response.data.response);
-      setCountries(response.data.response);
+      const storedCountries = localStorage.getItem('countries');
+      if (storedCountries) {
+        setCountries(JSON.parse(storedCountries));
+        setDataFetched(true); // Data already fetched
+      } else {
+        const apiKey = localStorage.getItem('apiKey');
+        console.log('Fetching countries...');
+        const response = await axios.get('https://v3.football.api-sports.io/countries', {
+          headers: {
+            'x-rapidapi-host': 'v3.football.api-sports.io',
+            'x-rapidapi-key': apiKey || '',
+          },
+        });
+        console.log('Countries:', response.data.response);
+        setCountries(response.data.response);
+        localStorage.setItem('countries', JSON.stringify(response.data.response));
+        setDataFetched(true); // Data fetched and stored
+      }
     } catch (error) {
       console.error('Error fetching countries:', error);
     }
@@ -50,25 +49,29 @@ const Leagues = ({ setLeagueId }) => {
 
   const fetchLeagues = async (countryId) => {
     try {
-      const apiKey = localStorage.getItem('apiKey');
-      console.log('Fetching leagues...');
-      const response = await axios.get('https://v3.football.api-sports.io/leagues', {
-        params: {
-          country: countryId,
-        },
-        headers: {
-          'x-rapidapi-host': 'v3.football.api-sports.io',
-          'x-rapidapi-key': apiKey || '',
-        },
-      });
-      console.log('Leagues:', response.data.response);
-      setLeagues(response.data.response);
+      const storedLeagues = localStorage.getItem(`leagues-${countryId}`);
+      if (storedLeagues) {
+        setLeagues(JSON.parse(storedLeagues));
+      } else {
+        const apiKey = localStorage.getItem('apiKey');
+        console.log('Fetching leagues...');
+        const response = await axios.get('https://v3.football.api-sports.io/leagues', {
+          params: {
+            country: countryId,
+          },
+          headers: {
+            'x-rapidapi-host': 'v3.football.api-sports.io',
+            'x-rapidapi-key': apiKey || '',
+          },
+        });
+        console.log('Leagues:', response.data.response);
+        setLeagues(response.data.response);
+        localStorage.setItem(`leagues-${countryId}`, JSON.stringify(response.data.response));
+      }
     } catch (error) {
       console.error('Error fetching leagues:', error);
     }
   };
-
-  
 
   const handleCountryChange = (event) => {
     setSelectedCountry(event.target.value);
@@ -81,15 +84,19 @@ const Leagues = ({ setLeagueId }) => {
 
   return (
     <div>
-      <h1>Contries</h1>
-      <select value={selectedCountry} onChange={handleCountryChange}>
-        <option value="">Select a country</option>
-        {countries.map((country) => (
-          <option key={country.id} value={country.id}>
-            {country.name}
-          </option>
-        ))}
-      </select>
+      <h1>Countries</h1>
+      {!dataFetched ? (
+        <p>Loading...</p>
+      ) : (
+        <select value={selectedCountry} onChange={handleCountryChange}>
+          <option value="">Select a country</option>
+          {countries.map((country) => (
+            <option key={country.id} value={country.id}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+      )}
       {leagues.length > 0 && (
         <div>
           <h2>Leagues</h2>
