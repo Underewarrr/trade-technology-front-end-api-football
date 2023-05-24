1462d8de906201b51a06bfd5e9d39d2aa0b18d8d
Underewarrr <rafhaelxd@gmail.com>
2023-05-23 22:21:06 -0300

    Refac: Logica para pegar Info do time

--- src/pages/user/panel/team/TeamInfo.jsx
+++ src/pages/user/panel/team/TeamInfo.jsx
@@ -1,51 +1,65 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../../../components/Header';
-import TeamStatistics from './TeamStatistics';
 import ProtectedRoute from '../../../../hoc/component/ProtectedRoute';
-// To use as props pass as parameter { currentTeamId } and receive in componenet.
+import TeamStatistics from './TeamStatistics';
+import { Card, Image } from 'react-bootstrap'; // Import the Card and Image components from React-Bootstrap
+
 const TeamInfo = () => {
   const [teamInfo, setTeamInfo] = useState({});
+  const [selectedLeague, setSelectedLeague] = useState('');
 
   const currentTeamId = parseInt(localStorage.getItem('currentTeam'));
-    console.log('currentTeamID', currentTeamId)
 
   useEffect(() => {
     // Fetch and set the team information from localStorage
-    const teamsData = localStorage.getItem(`teams-73`); // Replace '73' with the appropriate league ID
-    console.log(teamsData)
+    const teamsData = localStorage.getItem(`teams-${selectedLeague}`);
     if (teamsData) {
       const teams = JSON.parse(teamsData);
-        console.log('teams', teams)
       const currentTeam = teams.find((team) => team.team.id === currentTeamId);
       if (currentTeam) {
         setTeamInfo(currentTeam.team);
       }
     }
-  }, [currentTeamId]);
+  }, [currentTeamId, selectedLeague]);
+
+  useEffect(() => {
+    // Retrieve the selectedLeague from localStorage
+    const storedSelectedLeague = localStorage.getItem('selectedLeague');
+    if (storedSelectedLeague) {
+      setSelectedLeague(storedSelectedLeague);
+    }
+  }, []);
 
   return (
-    <><Header />
-    <ProtectedRoute />
-    <div>
-          <h1>Team Information</h1>
-          <div className="card">
-              <div className="card-body">
-                  <h5 className="card-title">Team Name</h5>
-                  <p className="card-text">{teamInfo.name}</p>
-              </div>
-          </div>
-          <div className="card">
-              <div className="card-body">
-                  <h5 className="card-title">Country</h5>
-                  <p className="card-text">{teamInfo.country}</p>
-              </div>
-          </div>
-          {/* Add more card components for additional team information */}
+    <>
+      <Header />
+      <ProtectedRoute />
+      <div>
+        <h1>Team Information</h1>
+        <Card>
+          <Card.Body>
+            <Card.Title>Team Name</Card.Title>
+            <Card.Text>{teamInfo.name}</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Title>Country</Card.Title>
+            <Card.Text>{teamInfo.country}</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Title>Logo</Card.Title>
+            <Image src={teamInfo.logo} alt={teamInfo.name} width={128} height={128} rounded /> {/* Display the logo as an image with a size of 128x128 pixels */}
+          </Card.Body>
+        </Card>
+        {/* Add more Card components for additional team information */}
       </div>
       <div>
-        <TeamStatistics />
+        <TeamStatistics teamId={currentTeamId} selectedLeague={selectedLeague} />
       </div>
-      </>
+    </>
   );
 };
 
