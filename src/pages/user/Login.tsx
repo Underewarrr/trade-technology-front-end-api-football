b04d585e15dfdeef7c85b08fe6ac26c3e52e8319
Underewarrr <rafhaelxd@gmail.com>
2023-05-25 15:06:44 -0300

    Fix: Partes não usadas

--- src/pages/user/Login.tsx
+++ src/pages/user/Login.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { setKey } from '../../services/requests/request-football-api';
 
-import { Alert, Container, Button, Card, Form } from 'react-bootstrap';
+import { Container, Button, Card, Form } from 'react-bootstrap';
 import { Navigate } from 'react-router-dom';
 import Header from '../components/Header';
 
