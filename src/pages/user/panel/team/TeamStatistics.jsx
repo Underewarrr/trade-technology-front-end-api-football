ae75689a4f32fdd1e465d7d418ae4cc7eaf5dfb4
Underewarrr <rafhaelxd@gmail.com>
2023-05-25 17:16:02 -0300

    Dev: Renderiza o grafico de gols por hora

--- src/pages/user/panel/team/TeamStatistics.jsx
+++ src/pages/user/panel/team/TeamStatistics.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
-
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Bar } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
 const TeamStatistics = ({ teamId, selectedLeague }) => {
   const [teamStatistics, setTeamStatistics] = useState();
+  Chart.register(...registerables);
 
   useEffect(() => {
     const storedTeamStatistics = localStorage.getItem(`teamStatistics-${teamId}`);
+    console.log("Team Statistics found in local storage");
     if (storedTeamStatistics) {
       setTeamStatistics(JSON.parse(storedTeamStatistics));
     } else {
@@ -17,6 +22,7 @@ const TeamStatistics = ({ teamId, selectedLeague }) => {
     const selectedSeason = localStorage.getItem('selectedSeason');
 
     try {
+      console.log("Fetching team statistics...");
       const storedTeamStatistics = localStorage.getItem(`teamStatistics-${teamId}`);
       if (!storedTeamStatistics) {
         const response = await fetch(
@@ -32,6 +38,7 @@ const TeamStatistics = ({ teamId, selectedLeague }) => {
 
         const data = await response.json();
         const teamStatisticsData = data.response;
+        console.log("Fetched team statistics:", teamStatisticsData);
         localStorage.setItem(`teamStatistics-${teamId}`, JSON.stringify(teamStatisticsData));
         setTeamStatistics(teamStatisticsData);
       }
@@ -40,28 +47,154 @@ const TeamStatistics = ({ teamId, selectedLeague }) => {
     }
   };
 
+  const renderGoalDistribution = () => {
+    const minuteDataFor = teamStatistics?.goals?.for?.minute;
+    const minuteDataAgainst = teamStatistics?.goals?.against?.minute;
+
+    if (minuteDataFor && minuteDataAgainst) {
+      const labels = Object.keys(minuteDataFor);
+      const dataFor = Object.values(minuteDataFor).map((data) => data.total);
+      const dataAgainst = Object.values(minuteDataAgainst).map((data) => data.total);
+
+      const chartData = {
+        labels,
+        datasets: [
+          {
+            label: 'For',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(75, 192, 192, 0.4)',
+            hoverBorderColor: 'rgba(75, 192, 192, 1)',
+            data: dataFor,
+          },
+          {
+            label: 'Against',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255, 99, 132, 0.4)',
+            hoverBorderColor: 'rgba(255, 99, 132, 1)',
+            data: dataAgainst,
+          },
+        ],
+      };
+
+      const chartOptions = {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      };
+
+      return (
+        <Card>
+          <Card.Body>
+            <Card.Title>Goal Distribution</Card.Title>
+            <Bar data={chartData} options={chartOptions} />
+          </Card.Body>
+        </Card>
+      );
+    }
+
+    return null;
+  };
+
   return (
     <div>
       <h2>Team Statistics</h2>
       {teamStatistics && teamStatistics.goals && (
-        <div>
-          <p>Goals For:</p>
-          <ul>
-            <li>Total: {teamStatistics.goals.for.total.home}</li>
-            <li>Home: {teamStatistics.goals.for.total.away}</li>
-            <li>Away: {teamStatistics.goals.for.total.total}</li>
-          </ul>
-          <p>Goals Against:</p>
-          <ul>
-            <li>Total: {teamStatistics.goals.against.total.home}</li>
-            <li>Home: {teamStatistics.goals.against.total.away}</li>
-            <li>Away: {teamStatistics.goals.against.total.total}</li>
-          </ul>
-          <p>Wins: {teamStatistics.fixtures.wins.total}</p>
-          <p>Losses: {teamStatistics.fixtures.loses.total}</p>
-          {/* Add more team statistics as needed */}
-        </div>
+        <Card>
+          <Card.Body>
+            <Card.Title>Goals</Card.Title>
+            <ListGroup variant="flush">
+              <ListGroup.Item>Total: {teamStatistics.goals.for.total.total}</ListGroup.Item>
+              <ListGroup.Item>Home: {teamStatistics.goals.for.total.home}</ListGroup.Item>
+              <ListGroup.Item>Away: {teamStatistics.goals.for.total.away}</ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
       )}
+      {teamStatistics && teamStatistics.goals && (
+        <Card>
+          <Card.Body>
+            <Card.Title>Goals Against</Card.Title>
+            <ListGroup variant="flush">
+              <ListGroup.Item>Total: {teamStatistics.goals.against.total.total}</ListGroup.Item>
+              <ListGroup.Item>Home: {teamStatistics.goals.against.total.home}</ListGroup.Item>
+              <ListGroup.Item>Away: {teamStatistics.goals.against.total.away}</ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+      )}
+      {teamStatistics && teamStatistics.fixtures && (
+        <Card>
+          <Card.Body>
+            <Card.Title>Wins and Losses</Card.Title>
+            <ListGroup variant="flush">
+              <ListGroup.Item>Wins: {teamStatistics.fixtures.wins.total}</ListGroup.Item>
+              <ListGroup.Item>Losses: {teamStatistics.fixtures.loses.total}</ListGroup.Item>
+              <ListGroup.Item>Draws: {teamStatistics.fixtures.draws.total}</ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+      )}
+      {teamStatistics && teamStatistics.lineups && (
+        <Card>
+          <Card.Body>
+            <Card.Title>Lineups</Card.Title>
+            <ListGroup variant="flush">
+              {Object.values(teamStatistics.lineups).map((lineupGroup, index) => (
+                <React.Fragment key={index}>
+                  {lineupGroup.formation && (
+                    <ListGroup.Item>
+                      Formation: {lineupGroup.formation}<br />
+                      Played: {lineupGroup.played}
+                    </ListGroup.Item>
+                  )}
+                  {lineupGroup.formation === undefined && (
+                    <ListGroup.Item>
+                      Formation: N/A<br />
+                      Played: N/A
+                    </ListGroup.Item>
+                  )}
+                </React.Fragment>
+              ))}
+            </ListGroup>
+          </Card.Body>
+        </Card>
+      )}
+      {teamStatistics && teamStatistics.cards && (
+        <Card>
+          <Card.Body>
+            <Card.Title>Cards</Card.Title>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                Yellow:
+                <ListGroup>
+                  {Object.entries(teamStatistics.cards.yellow).map(([timeRange, cardData]) => (
+                    <ListGroup.Item key={timeRange}>
+                      {timeRange}: {cardData.total} ({cardData.percentage})
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Red:
+                <ListGroup>
+                  {Object.entries(teamStatistics.cards.red).map(([timeRange, cardData]) => (
+                    <ListGroup.Item key={timeRange}>
+                      {timeRange}: {cardData.total} ({cardData.percentage})
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+      )}
+      {renderGoalDistribution()}
     </div>
   );
 };
