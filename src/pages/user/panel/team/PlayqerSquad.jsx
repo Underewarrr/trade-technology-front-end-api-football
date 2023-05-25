61416da5d8f613d77a2bd036266275d53b9bd86c
Underewarrr <rafhaelxd@gmail.com>
2023-05-25 14:01:42 -0300

    Dev: Adiciona Jest como dependencia de desenvolvimento

--- src/pages/user/panel/team/PlayqerSquad.jsx
+++ src/pages/user/panel/team/PlayqerSquad.jsx
@@ -4,31 +4,38 @@ const PlayerList = ({ teamId }) => {
   const [squad, setSquad] = useState([]);
 
   useEffect(() => {
-    const fetchSquad = async () => {
-      const apiKey = localStorage.getItem('apiKey');
-
-      try {
-        const response = await fetch(
-          `https://v3.football.api-sports.io/players/squads?team=${teamId}`,
-          {
-            method: 'GET',
-            headers: {
-              'x-rapidapi-host': 'v3.football.api-sports.io',
-              'x-rapidapi-key': apiKey || '',
-            },
-          }
-        );
-
-        const data = await response.json();
-        console.log('Squad:', data);
-        setSquad(data.response[0].players);
-      } catch (error) {
-        console.error('Error fetching squad:', error);
-      }
-    };
-
-    fetchSquad();
+    const storedSquad = JSON.parse(localStorage.getItem('squad'));
+    console.log("Stored Squad Found in LocalStorage", storedSquad)
+    if (storedSquad && storedSquad.length > 0) {
+      setSquad(storedSquad);
+    } else {
+      fetchSquad();
+    }
   }, [teamId]);
+
+  const fetchSquad = async () => {
+    const apiKey = localStorage.getItem('apiKey');
+
+    try {
+      const response = await fetch(
+        `https://v3.football.api-sports.io/players/squads?team=${teamId}`,
+        {
+          method: 'GET',
+          headers: {
+            'x-rapidapi-host': 'v3.football.api-sports.io',
+            'x-rapidapi-key': apiKey || '',
+          },
+        }
+      );
+
+      const data = await response.json();
+      console.log('Squad:', data);
+      setSquad(data.response[0].players);
+      localStorage.setItem('squad', JSON.stringify(data.response[0].players));
+    } catch (error) {
+      console.error('Error fetching squad:', error);
+    }
+  };
 
   return (
     <div>
